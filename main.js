e879a1b11785abcda8735935694dcc90a9acb5d1
LuisVZ485 <juggerlavz23@gmail.com>
2022-06-21 20:16:31 -0500

    video header

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 let dayNight =document.querySelector('.dayNight');
 let menuToggle = document.querySelector('.menuToggle');
+let body = document.querySelector('body');
 
+
+dayNight.onclick = function(){
+    body.classList.toggle('dark');
+
+    dayNight.classList.toggle('active')
+
+
+}
+
+
+
+menuToggle.onclick= function(){
+    menuToggle.classList.toggle('active');
+
+}
 
